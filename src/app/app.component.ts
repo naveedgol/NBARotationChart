b647e7294e8f0941bc73db40c47059b82bedae3c
Naveed Golafshani <ngolafshani@live.ca>
2018-04-15 18:51:43 -0400

    Restructure player and rotation data, begin templating chart, add html reset stylings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,47 +8,91 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   rots: Rotation[] = []
-  constructor(
-    private http: HttpClient
-  ) {
-    this.getPbp().subscribe(
+  homePlayers = [];
+  visitingPlayers = [];
+
+  players: Map<number, Player> = new Map<number, Player>();
+
+  constructor( private http: HttpClient) {
+
+
+    this.getGameDetails().subscribe(
       data => {
-        for ( let q = 0; q < data['g']['pd'].length; ++q ) {
-          for ( let i = 0; i < data['g']['pd'][q]['pla'].length; ++i ) {
-            if ( data['g']['pd'][q]['pla'][i]['etype'] === 8 ) {
-              this.rots.push( new Rotation(
-                data['g']['pd'][q]['pla'][i]['de'].substring(1,4),
-                data['g']['pd'][q]['pla'][i]['de'].match('\\[.+\\]\\s([A-z]+)')[1],
-                data['g']['pd'][q]['pla'][i]['de'].match('\\[.+\\]\\s[A-z]+\\sSubstitution\\sreplaced\\sby\\s([A-z]+)')[1],
-                data['g']['pd'][q]['pla'][i]['cl'],
-                q
-              ) );
-            }
-          }
+        for( let player of data['g']['hls']['pstsg'] ) {
+          this.players[player['pid']] = new Player(player['fn'], player['ln'], [new Rotation(true, 48*60,0)]);
         }
-        console.log(this.rots);
+        console.log(Object.keys(this.players)); // court = starter
       }
     );
+
+    // this.getPbp().subscribe(
+    //   data => {
+    //     console.log(data);
+    //     for ( let q = 0; q < data['g']['pd'].length; ++q ) {
+    //       for ( let i = 0; i < data['g']['pd'][q]['pla'].length; ++i ) {
+    //         if ( data['g']['pd'][q]['pla'][i]['etype'] === 8 ) {
+    //           this.rots.push( new Rotation(
+    //             data['g']['pd'][q]['pla'][i]['de'].substring(1,4),
+    //             // data['g']['pd'][q]['pla'][i]['de'].match('\\[.+\\]\\s([A-z]+)')[1],
+    //             parseInt(data['g']['pd'][q]['pla'][i]['epid']),
+    //             // data['g']['pd'][q]['pla'][i]['de'].match('\\[.+\\]\\s[A-z]+\\sSubstitution\\sreplaced\\sby\\s([A-z]+)')[1],
+    //             data['g']['pd'][q]['pla'][i]['pid'],
+    //             data['g']['pd'][q]['pla'][i]['cl'],
+    //             q
+    //           ) );
+    //         }
+    //       }
+    //     }
+    //     console.log(this.rots);
+    //   }
+    // );
+
   }
 
   getPbp() {
     //'https://cors-anywhere.herokuapp.com/' + 'https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2017/scores/pbp/0021701225_full_pbp.json'
     return this.http.get('../assets/pbp.json');
   }
+
+  getGameDetails() {
+    //https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2017/scores/gamedetail/0021701225_gamedetail.json
+    return this.http.get('../assets/details.json');
+  }
+
+  getIter() {
+    return Object.keys(this.players)
+  }
+  //http://stats.nba.com/js/data/widgets/boxscore_breakdown_20180411.json
+}
+
+class Player {
+  firstName: string;
+  lastName: string;
+  rotations: Rotation[];
+
+  constructor(
+    firstName: string,
+    lastName: string,
+    rotations: Rotation[]
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.rotations = rotations;
+  }
 }
 
 class Rotation {
-  team: string;
-  playerIn: string;
-  playerOut: string;
-  time: number;
-  quarter: number;
+  playing: boolean;
+  startTime: number;
+  endTime: number;
 
-  constructor( team: string, playerIn: string, playerOut: string, time: number, quarter: number) {
-    this.team = team;
-    this.playerIn = playerIn;
-    this.playerOut = playerOut;
-    this.time = time;
-    this.quarter = quarter;
+  constructor(
+    playing: boolean,
+    startTime: number,
+    endTime: number = -1
+  ) {
+    this.playing = playing;
+    this.startTime = startTime;
+    this.endTime = endTime;
   }
 }
